53cafb39f4ee264056e333ff5558401d3dfef06f
Jessica-malau <jessicarachel@students.polmed.ac.id>
2025-02-26 16:24:58 +0700

    Update terbaru

---
+++ file_/home.php
@@ -0,0 +1,104 @@
+<?php include'./_partial/_template/header.php';?>
+
+  <div class="container mt-4">
+    <div class="p-5 text-dark  d-flex align-items-center position-relative" style="border-top: 4px solid gold; 
+                  border-right: 20px solid gold; 
+                  border-top-right-radius: 40px;
+                  border-radius: 0 0 50px 10px;">
+
+      <!-- Image on the left -->
+      <!-- <img src="image/robot-ai.png" alt="AI Robot" class="img-fluid"
+        style="width: 200px; height: auto; position: absolute; bottom: 10px; left: 20px;"> -->
+
+      <!-- Text Content -->
+      <div class="" >
+        <h1 style="font-size:60px;"><b>Starvee Engine Update v.3 (vee-3)</b></h1>
+        <p class="text-muted">Gunakan Futuristik AI chatbot di lokal server kamu,<br />Layanan Chat bot virtual yang
+          bisa kamu gunain untuk bisnis sehari-hari bisa integrasi langsung di aplikasi kamu</p>
+
+      </div>
+    </div>
+  </div>
+<!-- Features Section -->
+<div class="container mt-5">
+  <h2 class="mb-3 fw-bolder">Starvee High Capabilites</h2>
+  <p>Due to high request of AI, The next generation AI Chatbot has been created to be reusable in code optimization in Aurora Server</p>
+  <div class="row row-cols-1 row-cols-md-2 row-cols-lg-4 g-4">
+    
+    <!-- Feature 1 -->
+    <div class="col">
+      <div class="card p-3  bg-light" 
+           style="border-top: 4px solid gold; border-right: 4px solid orange; 
+                  border-radius: 0 0 50px 10px;">
+        <div class="text-center">
+        <img src="aset/atik.png" width="70">
+        <h5 class="mt-3"><b>AI Chatbot</b></h5>
+        </div>
+      </div>
+    </div>
+
+    <!-- Feature 2 -->
+    <div class="col">
+      <div class="card p-3 bg-light " 
+           style="border-top: 4px solid gold; border-right: 4px solid orange; 
+                  border-radius: 0 0 50px 10px;">
+        <div class="text-center">
+          <img src="aset/se.jpg" width="70">
+          <h5 class="mt-3"><b>Security</b></h5>
+       </div>
+        
+      </div>
+    </div>
+
+    <!-- Feature 3 -->
+    <div class="col">
+      <div class="card p-3 bg-light " 
+           style="border-top: 4px solid gold; border-right: 4px solid orange; 
+                  border-radius: 0 0 50px 10px;">
+        <div class="text-center">
+          <img src="aset/fas.png" width="90">
+          <h5 class="mt-4"><b>Fastest Response</b></h5>
+          </div>
+    </div>
+    </div>
+
+    <!-- Feature 4 -->
+    <div class="col">
+      <div class="card p-3 bg-light " 
+           style="border-top: 4px solid gold; border-right: 4px solid orange; 
+                  border-radius: 0 0 50px 10px;">
+       <div class="text-center">
+        <img src="aset/lo.png" width="70">
+        <h5 class="mt-2"><b>Cloud Options<s</b></h5>
+        </div>
+    </div>
+    </div>
+    </div>
+
+  </div>
+</div>
+  </div>
+
+  <footer class="bg-primary text-white text-center ">
+    <div class="container">
+        <p>&copy; OUR SOCIAL MEDIA</p>
+        <div>
+            <a href="https://www.instagram.com" target="_blank" class="text-white mx-2">
+                <i class="fab fa-instagram fa-2x"></i>
+            </a>
+            <a href="https://www.facebook.com" target="_blank" class="text-white mx-2">
+                <i class="fab fa-facebook fa-2x"></i>
+            </a>
+            <a href="https://twitter.com" target="_blank" class="text-white mx-2">
+                <i class="fab fa-twitter fa-2x"></i>
+            </a>
+        </div>
+    </div>
+</footer>
+
+  <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js"
+    integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz"
+    crossorigin="anonymous"></script>
+</body>
+
+</html>
